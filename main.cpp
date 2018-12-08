401559760563568afd245554305e41f2c819c74f
Nikitha <nikipnr@gmail.com>
2018-12-07 22:45:56 -0600

    added select algorithm using median of Medians as pivot tested it working fine

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 #include "Partition.h"
 #include "Display.h"
 #include "RandomSelectAlgo.h"
-
+#include "SelectAlgoMedianAsPivot.h"
 using namespace std;
 
 int main()
 {
-    vector<int> array = {1, 4 ,3};
+    vector<int> array = {1, 2,3,4,5};
     MedianOfMedians m;
-    cout<<"Median: "<<m.median_Of_Medians(array, 0, array.size()-1, 2)<<endl;
+    cout<<"Median Of Medians: "<<m.median_Of_Medians(array, 0, array.size()-1, 2)<<endl;
     Partition p;
     Display d;
     d.display(array, 0, array.size()-1);
@@ -19,6 +19,10 @@ int main()
     d.display(array, 0, array.size()-1);
     cout<<10/2<<endl;
     RandomSelectAlgo r;
-    cout<<"Median: "<<r.randomised_Select(array, 0, array.size()-1, 1);
+    cout<<"Median: "<<r.randomised_Select(array, 0, array.size()-1, 1)<<endl;
+
+    SelectAlgoMedianAsPivot s;
+    cout<<"Third Element: "<<s.select_Algo(array, 0, array.size()-1, 2)<<endl;
+
     return 0;
 }
